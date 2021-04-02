4882f1fc05389e8d1f6cfafbeead871e20c3bdc1
ltto <ltt@ltts-MacBook-Pro.local>
2021-04-02 11:33:06 +0800

    start log

--- new.go
+++ new.go
@@ -3,6 +3,7 @@ package selenium
 import (
 	"fmt"
 	"github.com/tebeka/selenium"
+	"log"
 )
 
 func NewChromeDriver() (d *WebDriver, err error) {
@@ -16,6 +17,7 @@ func NewChromeDriver() (d *WebDriver, err error) {
 	if service, err = selenium.NewChromeDriverService(FindPATH("chromedriver"), port); nil != err {
 		return nil, fmt.Errorf("start a chromedriver service falid %v", err)
 	}
+	log.Printf("start a chromedriver service on 127.0.0.1:%v", port)
 	if driver, err := selenium.NewRemote(map[string]interface{}{"browserName": "chrome"}, fmt.Sprintf("http://localhost:%d/wd/hub", port)); err != nil {
 		service.Stop()
 		return nil, fmt.Errorf("failed to open session: %v", err)
